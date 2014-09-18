2f62414a17042acc42561552c137a1995c70fe0d
Adam Bryzak <adam@bryzak.net>
2014-09-18 20:24:05 +1000

    Full path to named-checkconf utility

--- update-named-slaves.sh
+++ update-named-slaves.sh
@@ -24,7 +24,7 @@ zone "$zone" {
 EOF
 done >> "$tmpfile" < <(curl --fail -sS -F token="$admin_token" "https://my-dns.org/api/zones" | jq '.zones[]' | sed -e 's/^"//'  -e 's/"$//')
 
-named-checkconf "$tmpfile"
+/usr/sbin/named-checkconf "$tmpfile"
 
 mv "$tmpfile" "/etc/bind/named.conf.mydns"
 service bind9 reload >/dev/null
